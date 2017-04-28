4a6b82e97f103d99a6e16ee34927c0c6c046a502
DESKTOP-LM5QMON\CGG <kisay97@naver.com>
2017-04-29 07:32:11 +0900

    NonPlayer 작성

---
+++ Assets/Script/NonPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class NonPlayer : GameActor {
+
+    private Animator animator;
+    private float playSpeed;
+
+	protected override void InitActor ()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected override void UpdateActor()
+    {
+        if(animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlayAnimation()
+    {
+        animator.speed = playSpeed;
+    }
+
+    public void SetPlaySpeed(float speed)
+    {
+        playSpeed = speed;
+    }
+
+    public void SetAnimatorController(RuntimeAnimatorController controller)
+    {
+        animator.runtimeAnimatorController = controller;
+    }
+
+    protected override void CallbackMessage(GameMessage message)
+    {
+        
+    }
+
+    
+}
