b76e0bb96b4b54000b1ac99a4fd5d5a9ec6f8d53
DESKTOP-LM5QMON\CGG <kisay97@naver.com>
2017-04-30 06:49:22 +0900

    새파일

---
+++ Assets/Script/UI/Popup/UIPopupUpgrade.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPopupUpgrade : MonoBehaviour
+{
+    public void InvincibilityButtonClick()
+    {
+        print("invincibility");
+        GameManager.Instance.PlayerActor.InvokePowerModeDurationUpdate();
+        GameManager.Instance.InvokePause(false);
+        gameObject.SetActive(false);
+    }
+
+    public void HpButtonClick()
+    {
+        print("hp");
+        GameManager.Instance.PlayerActor.MaxHealthPoint++;
+        GameManager.Instance.PlayerActor.InvokeHealthRecovery();
+        GameManager.Instance.InvokePause(false);
+        gameObject.SetActive(false);
+    }
+
+    public void AttackButton()
+    {
+        print("attack");
+        GameManager.Instance.PlayerActor.InvokeDamageUpdate();
+        GameManager.Instance.InvokePause(false);
+        gameObject.SetActive(false);
+    }
+}
